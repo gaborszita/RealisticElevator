5d91e172cc14ce4eb60df6754e01324a6fb2f927
Gabor Szita <gaborszita88@gmail.com>
2022-07-22 08:07:01 -0700

    Add commandrunner and commandlistener to main class

--- src/main/java/net/gaborszita/realisticelevator/RealisticElevator.java
+++ src/main/java/net/gaborszita/realisticelevator/RealisticElevator.java
@@ -2,6 +2,8 @@ package net.gaborszita.realisticelevator;
 
 import net.gaborszita.realisticelevator.commands.CommandListener;
 import net.gaborszita.realisticelevator.commands.commandmanager.CommandManager;
+import net.gaborszita.realisticelevator.commands.commandrunners.*;
+import net.gaborszita.realisticelevator.elevator.ElevatorManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -11,8 +13,23 @@ public class RealisticElevator extends JavaPlugin {
   public void onEnable() {
     getLogger().info("Loading RealisticElevator");
     CommandManager commandManager = new CommandManager();
+    ElevatorManager manager = new ElevatorManager(this);
+    commandManager.registerCommand(new AddDoorLever(manager));
+    commandManager.registerCommand(new AddFloor(this, manager));
+    commandManager.registerCommand(new AddFloorDoorLever(manager));
+    commandManager.registerCommand(new CreateElevator(manager));
+    commandManager.registerCommand(new DeleteElevator(manager));
+    commandManager.registerCommand(new ElevatorInfo(manager));
+    commandManager.registerCommand(new ListDoorLevers(manager));
+    commandManager.registerCommand(new ListElevators(manager));
+    commandManager.registerCommand(new ListFloorDoorLevers(manager));
+    commandManager.registerCommand(new ListFloors(manager));
+    commandManager.registerCommand(new RemoveDoorLever(manager));
+    commandManager.registerCommand(new RemoveFloor(manager));
+    commandManager.registerCommand(new RemoveFloorDoorLever(manager));
+    commandManager.registerCommand(new SetElevatorLocation(manager));
     Objects.requireNonNull(this.getCommand("elevator"))
-        .setExecutor(new CommandListener(this, commandManager));
+        .setExecutor(new CommandListener(commandManager));
     getLogger().info("RealisticElevator enabled");
   }
 

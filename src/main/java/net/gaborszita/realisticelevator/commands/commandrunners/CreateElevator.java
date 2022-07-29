78f78b48a4e2107925c8e8cd1f164292b32c090f
Gabor Szita <gaborszita88@gmail.com>
2022-07-29 15:40:02 -0700

    Add GNU GPLv3 license

--- src/main/java/net/gaborszita/realisticelevator/commands/commandrunners/CreateElevator.java
+++ src/main/java/net/gaborszita/realisticelevator/commands/commandrunners/CreateElevator.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2022 Gabor Szita
+ *
+ * This file is part of RealisticElevator.
+ *
+ * RealisticElevator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * RealisticElevator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with RealisticElevator. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package net.gaborszita.realisticelevator.commands.commandrunners;
 
 import net.gaborszita.realisticelevator.commands.commandmanager.CommandRunner;
